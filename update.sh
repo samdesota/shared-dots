62ce19c3566920c64ca91f5c34c7815eedd75d1c
Sam DeSota <me@samdesota.com>
2024-09-22 15:49:25 -0400

    reload hammerspoon

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "### Updating karabiner config ###"
 cd karabiner
 pnpm install
 pnpm run build
+
+echo "### Reloading hammerspoon ###"
+defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/shared/hammerspoon/init.lua"
+hs -c "hs.reload()"
